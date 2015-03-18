8c2f3d430c0b3ceb7f1e9b82853ec6a9cdb627a1
boran <simsekbo@gmail.com>
2015-03-18 14:09:46 +0100

    merhaba commit

---
+++ app/src/main/java/boransimsek/com/borandeneme/Merhaba.java
@@ -0,0 +1,12 @@
+package boransimsek.com.borandeneme;
+
+/**
+ * Created by boran.simsek on 3/18/2015.
+ */
+public class Merhaba {
+
+    public static String getMessage()
+    {
+        return "Hello";
+    }
+}
